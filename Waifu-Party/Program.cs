0fccc6385cbe08c48000ca2857378df3893c80cd
Victorious3 <vic@nightfall.moe>
2020-02-14 14:12:49 +0100

    Rename base file

--- Waifu-Party/Program.cs
+++ Waifu-Party/Program.cs
@@ -13,7 +13,7 @@ namespace Waifu_Party
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new WaifuParty())
                 game.Run();
         }
     }
--- Waifu-Party/WaifuParty.cs
+++ Waifu-Party/WaifuParty.cs
@@ -7,12 +7,12 @@ namespace Waifu_Party
     /// <summary>
     /// This is the main type for your game.
     /// </summary>
-    public class Game1 : Game
+    public class WaifuParty : Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         
-        public Game1()
+        public WaifuParty()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
